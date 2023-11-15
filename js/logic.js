21e15780b300924cb652ba23cbb2e19dbdad0925
ILoveC4t <109042057+ILoveC4t@users.noreply.github.com>
2023-11-15 15:06:06 +0100

    Generic Shop implemented

--- js/logic.js
+++ js/logic.js
@@ -4,11 +4,13 @@ game_bg_img.src = "assets/game_bg.png"
 const shop_bg_img = new Image()
 shop_bg_img.src = "assets/shop_bg.png"
 
-const Player = {
+const Player_Template = {
     w: 90,
     h: 30,
     x: 10,
     y: 240,
+    upgrade_cost: 20,
+    score_multiplier: 1,
     regen_rate_ps: 1,
     max_health: 100,
     health: 100,
@@ -20,10 +22,10 @@ const Player = {
     lastMove: 0,
     lastShoot: 0,
 }
-Player.img.src = Player.src
-let player = Object.assign({}, Player)
+Player_Template.img.src = Player_Template.src
+let Player = Object.assign({}, Player_Template)
 
-const Laser = {
+const Laser_Template = {
     w: 100,
     h: 30,
     x: 0,
@@ -32,14 +34,14 @@ const Laser = {
     cooldown: 5000,
     dps: 100,
     img: new Image(),
-    src: "assets/laser.png",
+    src: "assets/Laser.png",
     created: 0,
     inuse: false,
 }
-Laser.img.src = Laser.src
-let laser = Object.assign({}, Laser)
+Laser_Template.img.src = Laser_Template.src
+let Laser = Object.assign({}, Laser_Template)
 
-const Baddy = {
+const Baddy_Template = {
     w: 30,
     h: 30,
     x: 0,
@@ -57,9 +59,10 @@ const Baddy = {
         add_score(this.score)
     }
 }
-Baddy.img.src = Baddy.src
+Baddy_Template.img.src = Baddy_Template.src
+let Baddy = Object.assign({}, Baddy_Template)
 
-const Bullet = {
+const Bullet_Template = {
     w: 40,
     h: 20,
     x: 0,
@@ -69,9 +72,10 @@ const Bullet = {
     src: "assets/bullet.png",
     lastMove: 0,
 }
-Bullet.img.src = Bullet.src
+Bullet_Template.img.src = Bullet_Template.src
+let Bullet = Object.assign({}, Bullet_Template)
 
-const Boom = {
+const Boom_Template = {
     w: 50,
     h: 50,
     x: 0,
@@ -80,7 +84,8 @@ const Boom = {
     src: "assets/boom.png",
     created: 0,
 }
-Boom.img.src = Boom.src
+Boom_Template.img.src = Boom_Template.src
+let Boom = Object.assign({}, Boom_Template)
 
 const entities = {
     "Baddy": [Baddy, []],
@@ -94,8 +99,10 @@ const Button = {
     x: 0,
     y: 0,
     text: "BTN",
-    color : "red",
+    text_size: 30,
+    sub_text: null,
     text_color: "white",
+    color : "red",
     view: "game",
     callback: function() {},
 }
@@ -120,7 +127,6 @@ let next_baddy = 0
 
 let hiscore = 0
 let score = 0
-let score_multiplier = 1
 
 let paused = false
 let logic_cycle
@@ -168,7 +174,7 @@ function game_over_input_handler(e) {
         document.removeEventListener("keydown", game_over_input_handler)
         game_over_flag = false
         score = 0
-        player = Object.assign({}, Player)
+        Player = Object.assign({}, Player)
         main()
     }
 }
@@ -184,13 +190,13 @@ function game_over() {
     for (let [entity] of Object.entries(entities)) {
         entities[entity][1] = []
     }
-    laser.inuse = false
+    Laser.inuse = false
 }
 
 function spawnBullet() {
     const bullet = Object.assign({}, Bullet)
-    bullet.x = player.x + player.w + 5
-    bullet.y = player.y + player.h/2 - bullet.h/2
+    bullet.x = Player.x + Player.w + 5
+    bullet.y = Player.y + Player.h/2 - bullet.h/2
     entities["Bullet"][1].push(bullet)
 }
 
@@ -224,48 +230,62 @@ function check_collision(entity1, entity2) {
 function input_handler() {
     if (game_over_flag || paused) return
     for (const [key] of Object.entries(pressed_keys)) {
-        move_size = player.move_speed * (time_between_ticks())
+        move_size = Player.move_speed * (time_between_ticks())
         switch (key.toLowerCase()) {
             case " ":
-                if (player.lastShoot + player.shoot_delay > Date.now()) break
+                if (Player.lastShoot + Player.shoot_delay > Date.now()) break
                 spawnBullet()
-                player.lastShoot = Date.now()
+                Player.lastShoot = Date.now()
                 break
             case "w":
-                if (player.y - move_size < 0) break
-                player.y -= move_size
+                if (Player.y - move_size < 0) break
+                Player.y -= move_size
                 break
             case "s":
-                if (player.y + player.h + move_size > maxHeigth) break
-                player.y += move_size
+                if (Player.y + Player.h + move_size > maxHeigth) break
+                Player.y += move_size
                 break
             case "a":
-                if (player.x - move_size < 0) break
-                player.x -= move_size
+                if (Player.x - move_size < 0) break
+                Player.x -= move_size
                 break
             case "d":
-                if (player.x + player.w + move_size > Math.floor(maxWidth/5*4)) break
-                player.x += move_size
+                if (Player.x + Player.w + move_size > Math.floor(maxWidth/5*4)) break
+                Player.x += move_size
                 break
             case "q":
-                if (laser.inuse == false && score > 100 && laser.created + laser.duration + laser.cooldown < Date.now()) {
-                    laser.created = Date.now()
-                    laser.inuse = true
+                if (Laser.inuse == false && score > 100 && Laser.created + Laser.duration + Laser.cooldown < Date.now()) {
+                    Laser.created = Date.now()
+                    Laser.inuse = true
                     score -= 100
                 }
         }
     }
-    player.lastMove = Date.now()
+    Player.lastMove = Date.now()
 }
 
 function shop_view() {
+    ctx.font = "30px Arial"
+    ctx.fillStyle = "white"
+    ctx.strokeStyle = "red"
+    ctx.textAlign = "center"
+    ctx.fillText("Shop", maxWidth/2, 50)
+    ctx.strokeText("Shop", maxWidth/2, 50)
+    ctx.font = "20px Arial"
+    ctx.fillText("All Upgrades cost "+ Player.upgrade_cost +" Score", maxWidth/2, 80)
+    ctx.fillText("All Upgrades add +1%", maxWidth/2, 100)
+
+    ctx.textAlign = "left"
+    ctx.font = "30px Arial"
+    ctx.fillText("Score: " + Math.floor(score), 10, 40)
+    ctx.strokeText("Score: " + Math.floor(score), 10, 40)
 }
 
 function game_view() {
-    if (laser.inuse) {
-        ctx.drawImage(laser.img,laser.x,laser.y,laser.w,laser.h)
+    if (Laser.inuse) {
+        ctx.drawImage(Laser.img,Laser.x,Laser.y,Laser.w,Laser.h)
     }
-    ctx.drawImage(player.img,player.x,player.y,player.w,player.h)
+    ctx.drawImage(Player.img,Player.x,Player.y,Player.w,Player.h)
 
     for (const [entity, [entity_obj, entity_arr]] of Object.entries(entities)) {
         for (let i = 0; i < entity_arr.length; i++) {
@@ -276,33 +296,34 @@ function game_view() {
 
     ctx.font = "30px Arial"
     ctx.textAlign = "left"
+    ctx.strokeStyle = "black"
     ctx.strokeText("Score: " + Math.floor(score), 10, 70)
     
     ctx.fillStyle = "black"
     ctx.fillRect(10, 10, 200, 30)
     ctx.fillStyle = "red"
-    ctx.fillRect(12, 12, 196 * (player.health/100), 26)
+    ctx.fillRect(12, 12, 196 * (Player.health/100), 26)
 
     ctx.font = "20px Arial"
     ctx.fillStyle = "white"
     ctx.textAlign = "center"
-    if (player.health < 0) player.health = 0
-    const health_text = Math.ceil(player.health) + "/" + player.max_health
+    if (Player.health < 0) Player.health = 0
+    const health_text = Math.ceil(Player.health) + "/" + Player.max_health
     ctx.fillText(health_text, 110, 32)
 
     ctx.textAlign = "right"
     ctx.strokeText("Hiscore: " + hiscore, maxWidth-10, 30)
 
     ctx.textAlign = "left"
-    let cd = laser.created+laser.cooldown+laser.duration-Date.now()
-    let cd_text = "laser CD: "+ Math.ceil(cd/1000) + "s"
-    if (cd < 0) cd_text = "laser Ready"
-    if (laser.inuse) cd_text = "laser Active"
-    if (score < 100) cd_text = "laser requires 100 score"
+    let cd = Laser.created+Laser.cooldown+Laser.duration-Date.now()
+    let cd_text = "Laser CD: "+ Math.ceil(cd/1000) + "s"
+    if (cd < 0) cd_text = "Laser Ready"
+    if (Laser.inuse) cd_text = "Laser Active"
+    if (score < 100) cd_text = "Laser requires 100 score"
     ctx.strokeText(cd_text, 10, maxHeigth-10)
 
     if (game_over_flag) {
-        ctx.drawImage(Boom.img,player.x-player.h,player.y-player.w/2,player.w+50,player.w+50)
+        ctx.drawImage(Boom.img,Player.x-Player.h,Player.y-Player.w/2,Player.w+50,Player.w+50)
         ctx.font = "30px Arial"
         ctx.fillStyle = "red"
         ctx.textAlign = "center"
@@ -337,17 +358,22 @@ function draw() {
         if (button.view != view) continue
         ctx.fillStyle = button.color
         ctx.fillRect(button.x, button.y, button.w, button.h)
-        ctx.font = "30px Arial"
+        ctx.font = button.text_size + "px Arial"
         ctx.fillStyle = button.text_color
         ctx.textAlign = "center"
-        ctx.fillText(button.text, button.x + button.w/2, button.y + button.h/2 + 10)
+        if (!button.sub_text) ctx.fillText(button.text, button.x + button.w/2, button.y + button.h/2 + button.text_size/3)
+        else {
+            ctx.fillText(button.text, button.x + button.w/2, button.y + button.h/2 - button.text_size/3 + 5)
+            ctx.font = button.text_size/3*2 + "px Arial"
+            ctx.fillText(button.sub_text, button.x + button.w/2, button.y + button.h/2 + button.text_size/3*2 + 5)
+        }
     }
 
     requestAnimationFrame(draw)
 }
 
 function add_score(amount) {
-    score += amount*score_multiplier
+    score += Math.floor(amount*Player.score_multiplier)
 }
 
 function logic() {
@@ -357,7 +383,7 @@ function logic() {
         last_tick = current_tick
         return
     }
-    if (player.health <= 0) {
+    if (Player.health <= 0) {
         game_over()
         return
     }
@@ -366,22 +392,22 @@ function logic() {
     input_handler()
 
     //Player regen
-    if (player.health < player.max_health) {
-        player.health += player.regen_rate_ps * (time_between_ticks())
-        if (player.health > player.max_health) player.health = player.max_health
+    if (Player.health < Player.max_health) {
+        Player.health += Player.regen_rate_ps * (time_between_ticks())
+        if (Player.health > Player.max_health) Player.health = Player.max_health
     }
 
-    if (laser.inuse) {
-        laser.x = player.x + player.w + 10
-        laser.y = player.y + player.h / 2 - laser.h /2
-        if (laser.created + laser.duration < Date.now()) {
-            laser.inuse = false
+    if (Laser.inuse) {
+        Laser.x = Player.x + Player.w + 10
+        Laser.y = Player.y + Player.h / 2 - Laser.h /2
+        if (Laser.created + Laser.duration < Date.now()) {
+            Laser.inuse = false
         }
         for (let i = 0; i < entities["Baddy"][1].length; i++) {
             const baddy = entities["Baddy"][1][i]
-            //laser collision
-            if (check_collision(baddy, laser)) {
-                baddy.lives -= laser.dps * (time_between_ticks())
+            //Laser collision
+            if (check_collision(baddy, Laser)) {
+                baddy.lives -= Laser.dps * (time_between_ticks())
                 if (baddy.lives <= 0) {
                     baddy.die()
                     entities["Baddy"][1].splice(i,1)
@@ -422,10 +448,10 @@ function logic() {
             entities["Baddy"][1].splice(i,1)
             i--
         }
-        //player collision
-        if (check_collision(baddy, player)) {
-            player.health -= baddy.dps * (time_between_ticks())
-            baddy.lives -= player.dps * (time_between_ticks())
+        //Player collision
+        if (check_collision(baddy, Player)) {
+            Player.health -= baddy.dps * (time_between_ticks())
+            baddy.lives -= Player.dps * (time_between_ticks())
             console.log(baddy.lives)
             if (baddy.lives <= 0) {
                 baddy.die()
@@ -475,6 +501,46 @@ function exit_shop_callback() {
     draw()
 }
 
+const shop_buttons = {
+    "Health": Player.max_health+"HP",
+    "Regen": (Math.floor(Player.regen_rate_ps * 100) / 100)+"HP/s",
+    "Bullet DMG": Math.floor(Bullet.dps)+"DMG",
+    "Speed": (Math.floor(Player.move_speed * 100) / 100)+"px/s",
+    "Laser Duration": (Math.floor(Laser.duration*100)/100/1000)+"s",
+    "Score Multiplier": (Math.floor(Player.score_multiplier * 100) / 100),
+}
+function generic_stat_boost_callback() {
+    if (score < Player.upgrade_cost) return
+    switch(this.text) {
+        case "Health":
+            Player.max_health *= 1.01
+            this.sub_text = Math.floor(Player.max_health)+"HP"
+            break
+        case "Regen":
+            Player.regen_rate_ps *= 1.01
+            this.sub_text = (Math.floor(Player.regen_rate_ps * 100) / 100)+"HP/s"
+            break
+        case "Bullet DMG":
+            Bullet.dps *= 1.01
+            this.sub_text = Math.floor(Bullet.dps)+"DMG"
+            break
+        case "Speed":
+            Player.move_speed *= 1.01
+            this.sub_text = (Math.floor(Player.move_speed * 100) / 100)+"px/s"
+            break
+        case "Laser Duration":
+            Laser.duration *= 1.01
+            this.sub_text = (Math.floor(Laser.duration/1000*100)/100)+"s"
+            break
+        case "Score Multiplier":
+            Player.score_multiplier *= 1.01
+            this.sub_text = (Math.floor(Player.score_multiplier * 100) / 100)
+            break
+    }
+    score -= Player.upgrade_cost
+    Player.upgrade_cost = Math.ceil(Player.upgrade_cost * 1.1)
+}
+
 function setup_buttons() {
     const shop_btn = Object.assign({}, Button)
     shop_btn.x = maxWidth - shop_btn.w - 10
@@ -491,6 +557,35 @@ function setup_buttons() {
     exit_shop_btn.view = "shop"
     exit_shop_btn.callback = exit_shop_callback
     Buttons.push(exit_shop_btn)
+
+    const button_offset = 10
+    const cols = 3
+    const rows = 2
+    const btn_w = 200
+    const btn_h = 50
+    const w = button_offset*(cols-1)+btn_w*cols
+    const h = button_offset*(rows-1)+btn_h*rows
+
+    const x = maxWidth/2-w/2
+    const y = maxHeigth/2-h/2
+
+    let i = 0
+    for (const [button_text, button_subtext] of Object.entries(shop_buttons)) {
+        const button = Object.assign({}, Button)
+        button.x = x + (button_offset+btn_w)*(i%cols)
+        button.y = y + (button_offset+btn_h)*Math.floor(i/cols)
+        button.w = btn_w
+        button.h = btn_h
+        button.text = button_text
+        button.sub_text = button_subtext
+        button.text_size = 20
+        button.color = "red"
+        button.text_color = "white"
+        button.view = "shop"
+        button.callback = generic_stat_boost_callback
+        Buttons.push(button)
+        i++
+    }
 }
 
 function main() {
